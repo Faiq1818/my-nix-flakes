03c7a1b6f4a1107bcb588deef70ebb99e6f55bd9
faiq1818 <ghozyerlanggafaiq@gmail.com>
2025-06-12 17:43:15 +0700

    change nvf

--- nvf/nvf_module.nix
+++ nvf/nvf_module.nix
@@ -1,48 +1,77 @@
 { pkgs, lib, ... }:
 
 {
-  vim = {
-    theme = {
-      enable = true;
-      name = "gruvbox";
-      style = "dark";
-    };
-
-    visuals = {
-        indent-blankline.enable = true;
-        indent-blankline.setupOpts.scope.enabled = true;
-        indent-blankline.setupOpts.indent.smart_indent_cap = true;
-    };
-
-        statusline = { 
-                lualine.enable = true;
-                lualine.refresh.statusline = 50;
+    vim = {
+        theme = {
+            enable = true;
+            name = "catppuccin";
+            style = "mocha";
         };
 
-    telescope.enable = true;
-    autocomplete.nvim-cmp.enable = true;
-    filetree.neo-tree.enable = true;
-    lsp.enable = true;
-    tabline.nvimBufferline.enable = true;
+        keymaps = [
+            {
+                key = "<leader>e";
+                mode = "n";
+                silent = true;
+                action = ":Neotree position=right<CR>";
+            }
+            {
+                key = "<leader>w";
+                mode = "n";
+                silent = true;
+                action = "<C-w>";
+            }
+        ];
+
+        options = {
+            tabstop = 4;
+            shiftwidth = 4;
+        };
+
+        visuals = {
+            indent-blankline.enable = true;
+            indent-blankline.setupOpts.scope.enabled = true;
+            indent-blankline.setupOpts.indent.smart_indent_cap = true;
+        };
+
+        clipboard = {
+            enable = true;
+            providers.wl-copy.enable = true;
+            registers = "unnamedplus";
+        };
+
+        statusline = { 
+            lualine.enable = true;
+            lualine.refresh.statusline = 50;
+        };
+
+        telescope.enable = true;
+        autocomplete.nvim-cmp.enable = true;
+        filetree.neo-tree.enable = true;
+        filetree.neo-tree.setupOpts.enable_refresh_on_write = false;
+        lsp.enable = true;
+        tabline.nvimBufferline.enable = true;
         binds = {
-                whichKey.enable = true;
-                whichKey.setupOpts.preset = "helix";
+            whichKey.enable = true;
+            whichKey.setupOpts.preset = "helix";
         };
 
         terminal = { 
-                toggleterm.enable = true;
-                toggleterm.setupOpts.direction = "float";
+            toggleterm.enable = true;
+            toggleterm.setupOpts.direction = "float";
         };
 
-    languages = {
-      enableTreesitter = true;
-      nix.enable = true;
-      nix.lsp.enable = true;
-      nix.format.enable = true;
-      ts.enable = true;
-      ts.format.enable = true;
-      ts.lsp.enable = true;
-      rust.enable = true;
+        languages = {
+            enableTreesitter = true;
+            nix.enable = true;
+            nix.lsp.enable = true;
+            nix.format.enable = true;
+            nix.treesitter.enable = true;
+            ts.enable = true;
+            ts.format.enable = true;
+            ts.treesitter.enable = true;
+            ts.lsp.enable = true;
+            rust.enable = true;
+        };
     };
-  };
 }
