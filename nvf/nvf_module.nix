75ae6db16e668e824f6c64f8bf0abfe7605ce8d3
faiq1818 <ghozyerlanggafaiq@gmail.com>
2025-07-12 12:52:07 +0700

    change smth

--- nvf/nvf_module.nix
+++ nvf/nvf_module.nix
@@ -21,12 +21,42 @@
                 silent = true;
                 action = "<C-w>";
             }
+            {
+                key = "<leader>t";
+                mode = "n";
+                silent = true;
+                action = ":ToggleTerm<CR>";
+            }
+            {
+                key = "<Esc>";
+                mode = "t";
+                action = "<C-\\><C-n>";
+                silent = true;
+            }
+            {
+                key = "<leader>h";
+                mode = "n";
+                action = ":bprevious<CR>";
+                silent = true;
+            }
+            {
+                key = "<leader>j";
+                mode = "n";
+                action = ":bnext<CR>";
+                silent = true;
+            }
+            {
+                key = "<leader>i";
+                mode = "n";
+                action = ":bdelete<CR>";
+                silent = true;
+            }
         ];
 
-        options = {
-            tabstop = 4;
-            shiftwidth = 4;
-        };
+        #        options = {
+        #   tabstop = 4;
+        #   shiftwidth = 4;
+        #};
 
         visuals = {
             indent-blankline.enable = true;
@@ -51,6 +81,9 @@
         filetree.neo-tree.setupOpts.enable_refresh_on_write = false;
         lsp.enable = true;
         tabline.nvimBufferline.enable = true;
+        utility.sleuth.enable = true;
+
+
         binds = {
             whichKey.enable = true;
             whichKey.setupOpts.preset = "helix";
@@ -71,6 +104,8 @@
             ts.format.enable = true;
             ts.treesitter.enable = true;
             ts.lsp.enable = true;
+            tailwind.enable = true;
+            tailwind.lsp.enable = true;
             rust.enable = true;
         };
     };
