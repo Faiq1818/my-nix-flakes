c7a55d371decb8894faecc465ad2d1954f62d63f
faiq1818 <ghozyerlanggafaiq@gmail.com>
2025-07-29 22:38:40 +0700

    fixing delete current nvf

--- nvf/flake.nix
+++ nvf/flake.nix
@@ -1,7 +1,6 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
     nvf.url = "github:notashelf/nvf";
   };
 
--- nvf/nvf_module.nix
+++ nvf/nvf_module.nix
@@ -1,112 +1,116 @@
-{ pkgs, lib, ... }:
-
 {
-    vim = {
-        theme = {
-            enable = true;
-            name = "catppuccin";
-            style = "mocha";
-        };
-
-        keymaps = [
-            {
-                key = "<leader>e";
-                mode = "n";
-                silent = true;
-                action = ":Neotree position=right<CR>";
-            }
-            {
-                key = "<leader>w";
-                mode = "n";
-                silent = true;
-                action = "<C-w>";
-            }
-            {
-                key = "<leader>t";
-                mode = "n";
-                silent = true;
-                action = ":ToggleTerm<CR>";
-            }
-            {
-                key = "<Esc>";
-                mode = "t";
-                action = "<C-\\><C-n>";
-                silent = true;
-            }
-            {
-                key = "<leader>h";
-                mode = "n";
-                action = ":bprevious<CR>";
-                silent = true;
-            }
-            {
-                key = "<leader>j";
-                mode = "n";
-                action = ":bnext<CR>";
-                silent = true;
-            }
-            {
-                key = "<leader>i";
-                mode = "n";
-                action = ":bdelete<CR>";
-                silent = true;
-            }
-        ];
-
-        #        options = {
-        #   tabstop = 4;
-        #   shiftwidth = 4;
-        #};
-
-        visuals = {
-            indent-blankline.enable = true;
-            indent-blankline.setupOpts.scope.enabled = true;
-            indent-blankline.setupOpts.indent.smart_indent_cap = true;
-        };
-
-        clipboard = {
-            enable = true;
-            providers.wl-copy.enable = true;
-            registers = "unnamedplus";
-        };
-
-        statusline = { 
-            lualine.enable = true;
-            lualine.refresh.statusline = 50;
-        };
-
-        telescope.enable = true;
-        autocomplete.nvim-cmp.enable = true;
-        filetree.neo-tree.enable = true;
-        filetree.neo-tree.setupOpts.enable_refresh_on_write = false;
-        lsp.enable = true;
-        tabline.nvimBufferline.enable = true;
-        utility.sleuth.enable = true;
-
-
-        binds = {
-            whichKey.enable = true;
-            whichKey.setupOpts.preset = "helix";
-        };
-
-        terminal = { 
-            toggleterm.enable = true;
-            toggleterm.setupOpts.direction = "float";
-        };
-
-        languages = {
-            enableTreesitter = true;
-            nix.enable = true;
-            nix.lsp.enable = true;
-            nix.format.enable = true;
-            nix.treesitter.enable = true;
-            ts.enable = true;
-            ts.format.enable = true;
-            ts.treesitter.enable = true;
-            ts.lsp.enable = true;
-            tailwind.enable = true;
-            tailwind.lsp.enable = true;
-            rust.enable = true;
-        };
+  pkgs,
+  lib,
+  ...
+}: {
+  vim = {
+    theme = {
+      enable = true;
+      name = "catppuccin";
+      style = "mocha";
     };
+
+    keymaps = [
+      {
+        key = "<leader>e";
+        mode = "n";
+        silent = true;
+        action = ":Neotree position=right<CR>";
+      }
+      {
+        key = "<leader>w";
+        mode = "n";
+        silent = true;
+        action = "<C-w>";
+      }
+      {
+        key = "<leader>t";
+        mode = "n";
+        silent = true;
+        action = ":ToggleTerm<CR>";
+      }
+      {
+        key = "<Esc>";
+        mode = "t";
+        action = "<C-\\><C-n>";
+        silent = true;
+      }
+      {
+        key = "<leader>h";
+        mode = "n";
+        action = ":bprevious<CR>";
+        silent = true;
+      }
+      {
+        key = "<leader>j";
+        mode = "n";
+        action = ":bnext<CR>";
+        silent = true;
+      }
+    ];
+
+    visuals = {
+      indent-blankline.enable = true;
+      indent-blankline.setupOpts.scope.enabled = true;
+      indent-blankline.setupOpts.indent.smart_indent_cap = true;
+    };
+
+    clipboard = {
+      enable = true;
+      providers.wl-copy.enable = true;
+      registers = "unnamedplus";
+    };
+
+    statusline = {
+      lualine.enable = true;
+      lualine.refresh.statusline = 50;
+    };
+
+    telescope.enable = true;
+    autocomplete.nvim-cmp.enable = true;
+    filetree.neo-tree.enable = true;
+    filetree.neo-tree.setupOpts.open_files_in_last_window = true;
+    #filetree.neo-tree.setupOpts.enable_refresh_on_write = false;
+
+    #filetree = {
+    #  nvimTree = {
+    #    enable = true;
+    #    mappings.toggle = "<leader>r";
+    #  };
+    #};
+
+    lsp.enable = true;
+
+    tabline = {
+      nvimBufferline.enable = true;
+      nvimBufferline.mappings.closeCurrent = "<leader>i";
+    };
+
+    utility.sleuth.enable = true;
+
+    binds = {
+      whichKey.enable = true;
+      whichKey.setupOpts.preset = "helix";
+    };
+
+    terminal = {
+      toggleterm.enable = true;
+      toggleterm.setupOpts.direction = "float";
+    };
+
+    languages = {
+      enableTreesitter = true;
+      nix.enable = true;
+      nix.lsp.enable = true;
+      nix.format.enable = true;
+      nix.treesitter.enable = true;
+      ts.enable = true;
+      ts.format.enable = true;
+      ts.treesitter.enable = true;
+      ts.lsp.enable = true;
+      tailwind.enable = true;
+      tailwind.lsp.enable = true;
+      rust.enable = true;
+    };
+  };
 }
