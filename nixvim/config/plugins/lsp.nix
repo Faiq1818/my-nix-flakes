98513a59e36e006d78c807fca520a82897abfe81
faiq1818 <ghozyerlanggafaiq@gmail.com>
2025-10-20 11:18:30 +0700

    add some lsp

--- nixvim/config/plugins/lsp.nix
+++ nixvim/config/plugins/lsp.nix
@@ -4,12 +4,27 @@
       enable = true;
       inlayHints = true;
       servers = {
-        gopls = {
-          enable = true;
-        };
-        ts_ls = {
-          enable = true;
-        };
+        gopls.enable = true;
+        ts_ls.enable = true;
+        html.enable = true;
+        tailwindcss.enable = true;
+        nixd.enable = true;
+      };
+      keymaps = {
+      diagnostic =
+	{
+	  "<leader>ln" = "goto_next";
+	  "<leader>lp" = "goto_prev";
+	  "<leader>le" = "open_float";
+	};
+	lspBuf = {  
+	"<leader>lK" = "hover";
+	"<leader>lf" = "format";
+	  "<leader>lr" = "references";
+	  "<leader>ld" = "definition";
+	  "<leader>li" = "implementation";
+	  "<leader>lt" = "type_definition";
+	};
       };
     };
   };
